5ce7432a05d58983420e76c27e415ceee73fd21a
Steve Barnegren <steve.barnegren@gmail.com>
2017-10-24 08:36:24 +0100

    Create buildable framework

---
+++ SBAutoLayout/SBAutoLayout/SBAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  SBAutoLayout.h
+//  SBAutoLayout
+//
+//  Created by Steve Barnegren on 24/10/2017.
+//  Copyright © 2017 Steve Barnegren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SBAutoLayout.
+FOUNDATION_EXPORT double SBAutoLayoutVersionNumber;
+
+//! Project version string for SBAutoLayout.
+FOUNDATION_EXPORT const unsigned char SBAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBAutoLayout/PublicHeader.h>
+
+
